2f77b3e3ae69e895200fe3cc30ea3f6910f31b6f
chaxiaomao <coker2333@gmail.com>
2026-03-11 00:59:21 +0800

    updated

---
+++ backend/models/c2/entity/cl/ProductionScheduleItemSearch.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace backend\models\c2\entity\cl;
+
+use common\models\c2\entity\ProductionScheduleItem;
+use common\models\c2\statics\ProductionScheduleState;
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\c2\entity\ProductionSchedule;
+use yii\db\Expression;
+
+/**
+ * ProductionScheduleSearch represents the model behind the search form about `common\models\c2\entity\ProductionSchedule`.
+ */
+class ProductionScheduleItemSearch extends ProductionScheduleItem
+{
+    public $total_production;
+    public $estimated_ship_date;
+    public $actual_ship_date;
+    public $occurrence_date;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'schedule_id', 'product_id', 'combination_id', 'measure_id', 'package_id', 'enter_product_id', 'position'], 'integer'],
+            [['created_at', 'updated_at', 'status', 'total_production', 'estimated_ship_date', 'actual_ship_date', 'occurrence_date'], 'safe'],
+            [['product_name', 'product_sku', 'product_label', 'product_value', 'combination_name', 'package_name', 'memo'], 'safe',],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = self::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'sort' => [
+                'sortParam' => $this->getSortParamName(),
+            ],
+            'pagination' => false,
+            // 'pagination' => [
+            //     'pageParam' => $this->getPageParamName(),
+            //     'pageSize' => 20,
+            // ],
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->alias('a');
+
+        $query->select([
+            'a.id',
+            'a.product_id',
+            'a.product_sku',
+            'a.product_name',
+            'a.product_label',
+            'a.product_value',
+            'a.production_sum',
+            'a.memo',
+            'ps.occurrence_date',
+            'ps.estimated_ship_date',
+            'ps.actual_ship_date',
+            new Expression('MAX(ps.estimated_ship_date) as estimated_ship_date'),
+            // new Expression('ANY_VALUE(a.product_sku) as product_sku'),
+            // new Expression('ANY_VALUE(a.product_name) as product_name'),
+            new Expression('SUM(a.production_sum) as total_production'),
+        ]);
+
+        $query->leftJoin('{{%production_schedule}} ps', 'ps.id=a.schedule_id');
+
+        $query->with('product.measure');
+
+        $query->where(['like', 'ps.label', 'cl'])
+            ->andWhere(['in', 'ps.state', [ProductionScheduleState::COMMIT, ProductionScheduleState::CALCULATION]]);
+
+        $query->groupBy('a.product_sku');
+
+        $query->orderBy(['estimated_ship_date' => SORT_DESC]);
+
+        $query
+            ->andFilterWhere(['like', 'product_sku', $this->product_sku])
+            ->andFilterWhere(['like', 'product_name', $this->product_name])
+            ->andFilterWhere(['like', 'product_label', $this->product_label])
+            ->andFilterWhere(['like', 'product_value', $this->product_value]);
+
+        // print_r($query->createCommand()->getRawSql());
+
+        return $dataProvider;
+    }
+
+    public function getPageParamName($splitor = '-')
+    {
+        $name = "ProductionSchedulePage";
+        return \Yii::$app->czaHelper->naming->toSplit($name);
+    }
+
+    public function getSortParamName($splitor = '-')
+    {
+        $name = "ProductionScheduleSort";
+        return \Yii::$app->czaHelper->naming->toSplit($name);
+    }
+}
---
+++ backend/modules/CL/views/product-schedule/_detail.php
@@ -0,0 +1,165 @@
+<?php
+
+use common\models\c2\entity\ProductionConsumption;
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+
+?>
+
+
+<?php
+
+$data = [];
+$product_schedules = [];
+
+/** @var $models ProductionConsumption */
+if ($models) {
+    foreach ($models as $model) {
+        $cost_amount = $model['need_sum'];
+
+        if (isset($data[$model['need_product_id']])) {
+            $cost_amount = $model['need_sum'] + $data[$model['need_product_id']]['cost_amount'];
+        }
+
+        $data[$model['need_product_id']] = [
+            'need_product_id' => $model['need_product_id'],
+            'need_product_sku' => $model['need_product_sku'],
+            'need_product_name' => $model['need_product_name'],
+            'need_product_label' => $model['need_product_label'],
+            'need_product_value' => $model['need_product_value'],
+            'measure' => $model['product']['measure']['name'],
+            'cost_amount' => $cost_amount,
+            'current_stock' => $model['product']['stock'],
+            // 'diff_amount' => '',
+        ];
+
+        // $product_schedules[$model['need_product_id']][] = [
+        //     'code' => $model['code'],
+        //     'label' => $model['label'],
+        //     'need_sum' => $model['need_sum'],
+        //     'measure' => $model['product']['measure']['name'],
+        // ];
+        $product_schedules[$model['need_product_id']][] = array_merge($model, [
+            'need_sum' => $model['need_sum'],
+            'measure' => $model['product']['measure']['name'],
+        ]);
+    }
+}
+
+
+?>
+
+<div class="order-item-detail">
+    <div class="table-responsive" style="background-color: #fff;width: 100%;">
+
+        <table class="table table-bordered table-hover" style="min-width:1400px;table-layout:fixed;">
+
+            <tr class="tc">
+                <td class="box60">物料编号</td>
+                <td class="box60">物料名称</td>
+                <td class="box60">标签</td>
+                <td class="box60">值</td>
+                <!--            <td class="box120">使用数量/单位</td>-->
+                <!--            <td class="box120">单位</td>-->
+                <!--        <td class="box120">当前物料库存</td>-->
+                <td style="width: 112px">需要物料总量</td>
+                <td style="width: 112px">现有库存</td>
+                <td style="width: 112px">差值</td>
+                <td style="width: 200px">关联订单</td>
+            </tr>
+
+            <?php
+
+            foreach ($data as $productid => $datum): ?>
+                <tr class="tc">
+                    <td class="success"><?= $datum['need_product_sku'] ?></td>
+                    <td class="success"><?= $datum['need_product_name'] ?></td>
+                    <td class="success"><?= $datum['need_product_label'] ?></td>
+                    <td class="success"><?= $datum['need_product_value'] ?></td>
+                    <!--                <td class="success">--><? //= $datum['need_number'] ?><!--</td>-->
+                    <!--                <td class="success">--><?php //= $datum['product']['measure']['name'] ?><!--</td>-->
+                    <td class="success"><?= $datum['cost_amount'] . ' ' . $datum['measure'] ?></td>
+                    <!--            <td class="success">--><? //= $datum['product']['stock'] ?><!--</td>-->
+                    <td class="success"><?= $datum['current_stock'] . ' ' . $datum['measure'] ?></td>
+                    <!--                <td class="success">-->
+                    <?php //= $datum['product']['stock'] - $datum['cost_amount'] ?><!--</td>-->
+                    <td class="success">
+                        <?php
+
+                        if ($datum['current_stock'] < $datum['cost_amount']) {
+                            echo "<span class='text-red'>" . ($datum['current_stock'] - $datum['cost_amount']) . $datum['measure'] . "</span>";
+                        } else {
+                            echo "<span class='text-green'>剩余" . ($datum['current_stock'] - $datum['cost_amount']) . $datum['measure'] . "</span>";
+                        }
+                        ?>
+                    </td>
+                    <td class="success">
+
+                        <!--                    <a role="button" data-toggle="collapse" href="#Expand_-->
+                        <?php //= $datum['need_product_id'] ?><!--" aria-expanded="false"-->
+                        <!--                       aria-controls="collapseExample">-->
+                        <!--                        <i class="glyphicon glyphicon-eye-open">查看</i>-->
+                        <!--                    </a>-->
+                        <a class="btn btn-link" role="button" data-toggle="collapse"
+                           href="#Expand_<?= $datum['need_product_id'] ?>" aria-expanded="false"
+                           aria-controls="collapseExample">查看 <span
+                                    class="badge"><?= count($product_schedules[$datum['need_product_id']]) ?></span></a>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td class="no-padding" colspan="8">
+                        <div class="collapse bg-warning" id="Expand_<?= $datum['need_product_id'] ?>">
+
+                            <table class="table table-bordered table-hover">
+
+                                <tr class="tc">
+                                    <td class="box60">订单编号</td>
+                                    <td class="box60">订单标签</td>
+                                    <td class="box60">下单日期</td>
+                                    <td class="box60">预计出货日期</td>
+                                    <td class="box60">实际出货日期</td>
+                                    <td class="box60">流程状态</td>
+                                    <td class="box60">备注</td>
+                                    <td class="box60">需要物料数量</td>
+                                </tr>
+
+                                <?php foreach ($product_schedules[$datum['need_product_id']] as $item): ?>
+                                    <tr class="tc">
+                                        <td><?= $item['code'] ?></td>
+                                        <td><?= $item['label'] ?></td>
+                                        <td><?= $item['occurrence_date'] ? date('Y-m', strtotime($item['occurrence_date'])) : '' ?></td>
+                                        <td><?= $item['estimated_ship_date'] ? date('Y-m', strtotime($item['estimated_ship_date'])) : '' ?></td>
+                                        <td><?= $item['actual_ship_date'] ? date('Y-m', strtotime($item['actual_ship_date'])) : '' ?></td>
+                                        <td><?= \common\models\c2\statics\ProductionScheduleState::getLabel($item['state']) ?></td>
+                                        <td><?= $item['memo'] ?></td>
+                                        <td><?= $item['need_sum'] . $datum['measure'] ?></td>
+                                    </tr>
+                                    <!--                                <p>-->
+                                    <!--                                    <span>订单编号：--><?php //= $item['code'] ?><!--</span><br/>-->
+                                    <!--                                    <span>订单标签：--><?php //= $item['label'] ?><!--</span><br/>-->
+                                    <!--                                    <span>需要物料数量：--><?php //= $item['need_sum'] . ' ' . $datum['measure'] ?><!--</span>-->
+                                    <!--                                </p>-->
+                                    <!--                                <hr/>-->
+                                <?php endforeach; ?>
+                            </table>
+
+                        </div>
+                    </td>
+
+                </tr>
+
+            <?php endforeach; ?>
+
+
+        </table>
+        <?php if (!empty($datetime) && empty($data)): ?>
+            <div class="alert">如找不到该月份订单数据，可能需操作一次计算物料。
+                <a class="text-red" href="/cl/pam/production-schedule">点击</a>
+            </div>
+        <?php endif; ?>
+    </div>
+
+</div>
+
